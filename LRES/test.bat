0fa03f85e107a34e63f636d71abbfd22f29b5efe
jakeershaik7704 <80830652+jakeershaik7704@users.noreply.github.com>
2021-04-14 16:32:32 +0530

    Update test.bat

--- LRES/test.bat
+++ LRES/test.bat
@@ -1,3 +1,3 @@
 Java -cp "C:\WINDOWS\system32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\TestAutomation_1\LRES;
 C:\WINDOWS\system32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\TestAutomation_1\LRES\lib\*"; 
-org.testng.TestNG C:\WINDOWS\system32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\TestAutomation_1\LRES\TestNG.xml
+C:\WINDOWS\system32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\TestAutomation_1\LRES\TestNG.xml
